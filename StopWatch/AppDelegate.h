2e4aa3a3bb9f9ad306de075e430bfcced471f984
thanksdanny <hj-07@163.com>
2016-02-25 00:39:39 +0800

    Initial Commit

---
+++ StopWatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StopWatch
+//
+//  Created by Danny Ho on 2/25/16.
+//  Copyright © 2016 thanksdanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StopWatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StopWatch
+//
+//  Created by Danny Ho on 2/25/16.
+//  Copyright © 2016 thanksdanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
